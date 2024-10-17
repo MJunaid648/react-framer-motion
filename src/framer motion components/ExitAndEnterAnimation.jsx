a44a9d07a35fe0b13dd30a230ae6d235646a1c86
Muhammad Junaid <648mjunaid@gmail.com>
2024-10-17 14:29:27 +0500

    exit and enter animation

--- src/framer motion components/ExitAndEnterAnimation.jsx
+++ src/framer motion components/ExitAndEnterAnimation.jsx
@@ -1,13 +1,37 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const ExitAndEnterAnimation = () => {
+  const [removeInnerCircle, setRemoveInnerCircle] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setRemoveInnerCircle(true);
+    }, 5000);
+  });
   return (
-    <div className="h-full w-full">
-      <motion.div className="w-4 h-8 bg-amber-500">
-
-      </motion.div>
+    <div className="">
+      <button
+        className=" bg-yellow-400 rounded-sm px-2 py-1 text-black font-s"
+        onClick={() => {
+          setRemoveInnerCircle((prev) => !prev);
+        }}
+      >
+        Toggle
+      </button>
+      <AnimatePresence>
+        {!removeInnerCircle && (
+          <motion.div
+            initial={{ opacity: 1, x: -400, y: 300 }}
+            animate={{ opacity: 1, x: 0, y: 0 }}
+            transition={{ duration: 3 }}
+            exit={{ opacity: 0, x: 400, y: 300, scale: 1.5 }}
+            className="h-32 w-32 bg-yellow-400 mx-auto rounded-full"
+          >
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
-  )
-}
+  );
+};
 
-export default ExitAndEnterAnimation
+export default ExitAndEnterAnimation;
